17ad75947a3b44eacf7938adae0c8255fc25ca34
aShu-guo <guochengli97@163.com>
2023-11-08 15:57:32 +0800

    feat: 新增deploy.yml

--- docs/.vitepress/config.mts
+++ docs/.vitepress/config.mts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vitepress';
 // 导出navs
-import MvvmNavs from './navs/mvvm';
+import MvvmNavs from './navs/ecology';
 // 导出sidebars
-import RfcTranslate from './sidebars/mvvm/rfc-translate';
-import Version2 from './sidebars/mvvm/version2';
-import Version3 from './sidebars/mvvm/version3';
+import RfcTranslate from './sidebars/ecology/vue/rfc-translate';
+import Version2 from './sidebars/ecology/vue/version2';
+import Version3 from './sidebars/ecology/vue/version3';
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: 'notes',
   description: 'A VitePress Site',
+  base: '/blog/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     outline: 'deep',
--- docs/.vitepress/sidebars/ecology/vue/version2.ts
+++ docs/.vitepress/sidebars/ecology/vue/version2.ts
@@ -1,27 +1,27 @@
 export default {
-  '/mvvm/vue-ecology/2.x/': [
+  '/ecology/vue/2.x/': [
     {
       text: '2.x版本',
       items: [
         {
           text: '前言',
-          link: '/mvvm/vue-ecology/2.x/',
+          link: '/ecology/vue/2.x/',
         },
         {
           text: 'APIs',
-          link: '/mvvm/vue-ecology/2.x/api',
+          link: '/ecology/vue/2.x/api',
         },
         {
           text: '响应式模块',
-          link: '/mvvm/vue-ecology/2.x/reactive-module',
+          link: '/ecology/vue/2.x/reactive-module',
         },
         {
           text: '编译模块',
-          link: '/mvvm/vue-ecology/2.x/compile-module',
+          link: '/ecology/vue/2.x/compile-module',
         },
         {
           text: 'Q&A',
-          link: '/mvvm/vue-ecology/2.x/Q&A',
+          link: '/ecology/vue/2.x/Q&A',
         },
       ],
     },
