0df00b6bb77a902d18d6411f6fffec4084b08997
aShu-guo <guochengli97@163.com>
2023-11-20 11:06:35 +0800

    feat: 新增反射

--- docs/.vitepress/sidebars/courses/computer-graphics.ts
+++ docs/.vitepress/sidebars/courses/computer-graphics.ts
@@ -43,6 +43,7 @@ export default {
             { text: '旋转', link: '/courses/computer-graphics/3d-math/7.1-chapter' },
             { text: '放缩', link: '/courses/computer-graphics/3d-math/7.2-chapter' },
             { text: '正交投影', link: '/courses/computer-graphics/3d-math/7.3-chapter' },
+            { text: '镜像', link: '/courses/computer-graphics/3d-math/7.4-chapter' },
           ],
         },
       ],
