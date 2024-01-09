5f335d9c0a5ba829c06598dfa1ef06e30690d037
aShu-guo <guochengli97@163.com>
2024-01-09 16:54:20 +0800

    feat: 新增js number ieee-754

--- docs/.vitepress/sidebars/base/js.ts
+++ docs/.vitepress/sidebars/base/js.ts
@@ -5,14 +5,16 @@ export default {
       items: [
         { text: '前言', link: '/base/js/' },
         { text: '词法结构', link: '/base/js/1-chapter' },
+        { text: '类型、值和变量', link: '/base/js/3-chapter' },
         { text: '表达式与操作符', link: '/base/js/4-chapter' },
-
       ],
     },
     {
       text: '拓展',
       items: [
         { text: 'delete关键字', link: '/base/js/delete' },
+        { text: '闭包与垃圾回收', link: '/base/js/closure与gc' },
+        { text: 'BigInt与IEEE-754', link: '/base/js/safe-number' },
       ],
     },
   ],
