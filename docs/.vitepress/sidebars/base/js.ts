ccf28f4932b989e2cfe46d95e240ad0b7152f73a
aShu-guo <guochengli97@163.com>
2024-01-25 11:45:30 +0800

    feat(JS): 新增词法结构章节

--- docs/.vitepress/sidebars/base/js.ts
+++ docs/.vitepress/sidebars/base/js.ts
@@ -4,7 +4,7 @@ export default {
       text: '重学JavaScript',
       items: [
         { text: '前言', link: '/base/js/' },
-        { text: '词法结构', link: '/base/js/1-chapter' },
+        { text: '词法结构', link: '/base/js/2-chapter' },
         { text: '类型、值和变量', link: '/base/js/3-chapter' },
         { text: '表达式与操作符', link: '/base/js/4-chapter' },
         { text: '模块', link: '/base/js/10-chapter' },
---
+++ docs/base/js/codes/test.js
@@ -0,0 +1,4 @@
+const café = 1;
+const café = 2;
+console.log(café);
+console.log(café);
