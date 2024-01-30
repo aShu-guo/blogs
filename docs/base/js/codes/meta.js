437b504b22b883c2380e2015fc79edca26d3c7f1
aShu-guo <guochengli97@163.com>
2024-01-30 14:36:47 +0800

    mod(JS): proxy章节补全示例代码

--- docs/base/js/codes/meta.js
+++ docs/base/js/codes/meta.js
@@ -3,11 +3,8 @@ let map = new Map();
 let proxy = new Proxy(map, {
   get(target, p, receiver) {
     let value = Reflect.get(...arguments);
-    // console.log('>>>>value:', value === target.get);
-    // console.log('>>>>value:', value === map.get);
-
     return typeof value === 'function' ? value.bind(target) : value;
   },
 });
 
-proxy.set('test', 1); // Error
+proxy.set('test', 1);
