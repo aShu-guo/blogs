ee1b25ee507f1cc0ba818fcc395a968ba870f453
aShu-guo <guochengli97@163.com>
2024-05-17 17:09:56 +0800

    feat: 添加评论功能

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,17 +3,44 @@ import DefaultTheme from 'vitepress/theme';
 import { onMounted, watch, nextTick } from 'vue';
 import { useData, useRoute } from 'vitepress';
 import mediumZoom from 'medium-zoom';
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { toRefs } from 'vue';
 
 import './index.css';
-import { initOml2d } from './utils';
 
 export default {
   ...DefaultTheme,
   async enhanceApp() {
-    initOml2d();
+    if (!import.meta.env.SSR) {
+      const { loadOml2d } = await import('oh-my-live2d');
+
+      loadOml2d({
+        menus: {
+          items: [],
+        },
+        statusBar: { disable: true },
+        tips: {
+          style: {
+            top: '-50px',
+          },
+        },
+        models: [
+          {
+            path: 'https://model.oml2d.com/cat-white/model.json',
+            // scale: 0.15,
+            position: [0, 20],
+            // stageStyle: {
+            //   height: 250,
+            // },
+          },
+        ],
+      });
+    }
   },
   setup() {
     const route = useRoute();
+
+    // 初始化图片查看器
     const initZoom = () => {
       // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' });
       mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
@@ -24,6 +51,37 @@ export default {
     watch(
       () => route.path,
       () => nextTick(() => initZoom()),
+    );
+
+    const { frontmatter } = toRefs(useData());
+    // Obtain configuration from: https://giscus.app/
+    giscusTalk({
+        repo: 'aShu-guo/giscus-discussions',
+        repoId: 'R_kgDOJThJwQ',
+        category: 'General', // default: `General`
+        categoryId: 'DIC_kwDOJThJwc4CVlrP',
+        mapping: 'title', // default: `pathname`
+        inputPosition: 'bottom', // default: `top`
+        // lang: 'en', // default: `zh-CN`
+        // i18n setting (Note: This configuration will override the default language set by lang)
+        // Configured as an object with key-value pairs inside:
+        // [your i18n configuration name]: [corresponds to the language pack name in Giscus]
+        locales: {
+          'zh-Hans': 'zh-CN',
+          'en-US': 'en',
+        },
+        // homePageShowComment: false, // Whether to display the comment area on the homepage, the default is false
+        // lightTheme: 'light', // default: `light`
+        // darkTheme: 'transparent_dark', // default: `transparent_dark`
+        // ...
+      }, {
+        frontmatter, route,
+      },
+      // Whether to activate the comment area on all pages.
+      // The default is true, which means enabled, this parameter can be ignored;
+      // If it is false, it means it is not enabled.
+      // You can use `comment: true` preface to enable it separately on the page.
+      true,
     );
   },
 };
