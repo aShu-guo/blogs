681ee5cdb36d46d981ffe1788164c032ea412470
aShu-guo <guochengli97@163.com>
2024-08-28 09:14:31 +0800

    feat: 新增

--- docs/.vitepress/sidebars/life/finance.ts
+++ docs/.vitepress/sidebars/life/finance.ts
@@ -6,14 +6,29 @@ export default {
       items: [
         {
           text: '396经济学综合',
+          link: '/life/finance/396/',
           items: [
             { text: '函数', link: '/life/finance/396/chapter-1' },
           ],
         },
         {
           text: '431金融学',
+          link: '/life/finance/431/',
           items: [
-            { text: '模拟1', link: '/life/finance/431/chapter-1' },
+            {
+              text: '公司理财',
+              link: '/life/finance/431/company-finance/',
+              items: [
+                {
+                  text: '概论',
+                  link: '/life/finance/431/company-finance/chapter-1',
+                },
+                {
+                  text: '企业经营能力分析',
+                  link: '/life/finance/431/company-finance/chapter-2',
+                },
+              ],
+            },
           ],
         },
       ],
